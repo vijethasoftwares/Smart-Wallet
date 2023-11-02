7c0222193545016738502714925bac3a56ebd87d
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-11-02 14:29:33 +0530

    connect inside db

--- config/db.js
+++ config/db.js
@@ -3,14 +3,14 @@ const { MongoClient } = require('mongodb');
 
 let db;
 
-
+const myUri=process.env.MONGODB_URI
 async function connectToDatabase(uri) {
 
     try {
-      if (!process.env.MONGODB_URI) {
+      if (!myUri) {
   throw new Error('No MongoDB URI provided. Please set the MONGODB_URI environment variable.');
 }
-      const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+      const client = new MongoClient(myUri, { useNewUrlParser: true, useUnifiedTopology: true });
       await client.connect();
       db = client.db(); // Specify your database name if it's different from the default
       console.log('Connected to MongoDB');
@@ -18,11 +18,13 @@ async function connectToDatabase(uri) {
       console.error('Error connecting to MongoDB:', error);
     }
   }
-  function getDB() {
+ async function getDB() {
+    const client = new MongoClient(myUri, { useNewUrlParser: true, useUnifiedTopology: true });
+      await client.connect();
+      db = client.db(); // Specify your database name if it's different from the default
     if (!db) {
       throw new Error('Database not connected');
     }
     return db;
   }
-  connectToDatabase(process.env.MONGODB_URI)
   module.exports = { connectToDatabase, getDB };
